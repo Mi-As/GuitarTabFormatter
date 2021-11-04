dd0fcff665df8c3edaae7c200bdd3467cbb8613a
Mi-As <m.astor@gmx.de>
2021-11-04 19:14:17 +0100

    default tab line formatting

--- ultimate_guitar_parser.py
+++ ultimate_guitar_parser.py
@@ -15,6 +15,11 @@ But at \[A]least I'm not alone
 \endverse
 """
 
+# chord_pattern = r'\[ch\]([A-h#785maj]*)\[/ch\]'
+chord_pattern = r'\[ch\](.*?)\[/ch\]'
+tabline_pattern = r'\[tab\](.*?)\[/tab\]'
+return_pattern = r'\\r\\n'
+
 def get_and_cut_html(tab_link):
 	req = Request(tab_link)	
 	html_page = urlopen(req)
@@ -34,29 +39,47 @@ def get_and_cut_html(tab_link):
 	#    file.write(tab_str)
 
 def format_tab_data_to_latex(tab_data):
-	
-	# format chords
-	chord_pattern = r'\[ch\]([A-h#785maj]*)\[/ch\]'
-	chord_re = re.sub(chord_pattern, r"\\[\1]", tab_data)
-	tab_data = chord_re
 
-	print(tab_data)
+	# format tablines
+	while(re.search(tabline_pattern, tab_data)):
+		new_tabline = join_chords_and_text(re.search(tabline_pattern, tab_data).group(1))
+		tabline_re = re.sub(tabline_pattern, new_tabline, tab_data, count=1)
+		tab_data = str(tabline_re)
 
 	# format line returns
 	return_pattern = r'\\r\\n'
 	return_re = re.split(return_pattern, tab_data)
 
 	with open("beautifulsoup.html", "w") as file:
-		for line in return_re:
-			file.write(str(line) + "\n")
+	 	for line in return_re:
+	 		file.write(str(line) + "\n")
+
+def join_chords_and_text(tabline):
+
+	
+	return_re = re.split(return_pattern, tabline)
+
+	if not len(return_re) == 2:
+		return "Error"
+
+	new_tabline = return_re[1] + 10*" "
+	tmp = 0
+	for match in re.finditer(chord_pattern, return_re[0]):
+
+		position = match.span()[0] + tmp
+		new_tabline = f'{new_tabline[:position]}\\[{match.group(1)}]{new_tabline[position:]}'
+
+		tmp += len(match.group(1))+3
+
+	return new_tabline.strip()
 
 
 if __name__ == '__main__':
 	
 	#tab_link = input("Enter the ultimate guitar web link für your tabs: ")
 
-	test = "https://tabs.ultimate-guitar.com/tab/boywithuke/two-moons-chords-3757571"
-	#test = "https://tabs.ultimate-guitar.com/tab/ed-sheeran/perfect-chords-1956589"
+	tab_link = "https://tabs.ultimate-guitar.com/tab/boywithuke/two-moons-chords-3757571"
+	# tab_link = "https://tabs.ultimate-guitar.com/tab/ed-sheeran/perfect-chords-1956589"
 
-	tab_data = get_and_cut_html(test)
+	tab_data = get_and_cut_html(tab_link)
 	format_tab_data_to_latex(tab_data)
